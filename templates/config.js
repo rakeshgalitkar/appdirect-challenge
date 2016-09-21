1b5d5bf56ff9e173a834e64415b989c766a840b8
Special <Special>
2016-09-21 22:49:17 +0530

    V2

---
+++ templates/config.js
@@ -0,0 +1,9 @@
+var config = {
+  consumerKey: "",
+  consumerSecret: "",
+  accessToken: "",
+  accessTokenSecret: "",
+  port: 7890
+};
+
+module.exports = config;
--- templates/server.js
+++ templates/server.js
@@ -12,5 +12,7 @@ exec('node ./express.js', function(error, stdout, stderr) {
     if (error !== null) {
         console.log('exec error: ', error);
     }
-    
 });
+
+console.log("HTTP server rinning on 8080");
+console.log("twitter-proxy server rinning on 7890");
